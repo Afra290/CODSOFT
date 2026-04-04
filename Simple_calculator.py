afdbf01dc3dcddf0043dad0ef3033aa1493d67f2
Afra290 <afrapore@gmail.com>
2026-04-04 21:52:03 +0530

    Add files via upload
    Task 2 Completed!
    Built a Basic Calculator using Python as my first assignment. This experience helped me understand the practical side of coding and building small applications.
    Looking forward to learning more and completing upcoming tasks!
    
    CodSoft 
    #Internship #Python 
    #CodSoft

---
+++ Simple_calculator.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed April 01 2026
+
+@author: Afra
+"""
+def your_calculator():
+    print("\n---Simple Calculator---")
+    while True: 
+         try:
+            n1=float(input("Enter 1st number:"))
+            op=input("Enter operator(+,-,*,\):")
+            n2=float(input("Enter 2nd number:"))
+        
+            if op=='+': 
+               print(f"Result:{n1+n2}")
+            elif op== '-':
+               print (f"Result:{n1-n2}")
+            elif op== '*': 
+               print (f"Result:{n1*n2}")
+            elif op== '/': 
+               print (f"Result:{n1*n2}" if n2!=0 else"Errror:  Div by 0")
+            else: print("Invalid Operator")
+         except ValueError :
+               print("Error: please enter numbers only")
+
+your_calculator()
